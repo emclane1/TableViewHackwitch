625b6b7fce8889663e7e397a4bbab60302afb1e9
EVERETT MCLANE <rl238087@rhs-macair-24.rlas-116.org>
2018-10-29 12:09:41 -0500

    Initial Commit

---
+++ TableViewHackwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewHackwitch
+//
+//  Created by EVERETT MCLANE on 10/29/18.
+//  Copyright © 2018 EVERETT MCLANE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
